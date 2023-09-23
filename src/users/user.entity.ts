7518dd207d0f55b9d9a4f417f44f2e53953ff001
Ogwalu Chidozie <77652058+gentomacine@users.noreply.github.com>
2023-09-23 07:07:00 +0100

    Add report entities and connect to root module

---
+++ src/users/user.entity.ts
@@ -0,0 +1,13 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+ export class User{
+    @PrimaryGeneratedColumn()
+    id: Number;
+
+    @Column()
+    email: String;
+
+    @Column()
+    password: string
+ }
