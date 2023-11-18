30f962353fc4765545cda710f9665fc0ae277fb0
Ogwalu Chidozie <77652058+gentomacine@users.noreply.github.com>
2023-11-18 11:50:53 +0100

    removing a user from database

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -22,8 +22,19 @@ export class UsersService {
 
     async update(id:number, attrs:Partial<User>){
         const user = await this.findOne(id)
+        if (!user){
+            throw new Error ("user not found")
+        };
+        Object.assign(user,attrs);
+        return this.repo.save(user)
     }
 
-    remove(){}
+    async remove(id:number){
+        const user = await this.findOne(id);
+        if (!user){
+            throw new Error("user not found");
+        };
+        return this.repo.remove(user)
+    }
 
 }
