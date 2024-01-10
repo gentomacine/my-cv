f5afade84d359ce99f49c11bb6ab6f008565dce8
Ogwalu Chidozie <77652058+gentomacine@users.noreply.github.com>
2024-01-10 08:52:26 +0100

    update user dto

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post, Get, Patch, Delete, Param, Query } from '@nestjs/common';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
 
 @Controller('auth')
 export class UsersController {
@@ -25,7 +26,9 @@ constructor(private usersService:UsersService){}
         return this.usersService.remove(parseInt(id));
     };
 
-    // @Patch('/:id')
-    // updateUser(@Param)
+     @Patch('/:id')
+     updateUser(@Param('id') id:string, @Body() body: UpdateUserDto){
+        return this.usersService.update(parseInt(id), body)
+     }
 
 }
